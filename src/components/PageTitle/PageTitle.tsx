01625f95fda9fbbe4699400c22216068d200082b
amberlynn <amberlynn364@gmail.com>
2024-05-19 19:36:37 +0300

    refactor: remove console.log

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -7,7 +7,6 @@ interface PageTitleProps {
 
 // eslint-disable-next-line react/display-name
 const PageTitle = memo(({ textContent }: PageTitleProps) => {
-  console.log('page title render');
   return (
     <>
       <Typography variant="h5" textAlign="center" p={2}>
